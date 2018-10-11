62e1d3add0a2687b1fb4681ad4644f96b479c428
chmakalexandr <chmak.av@gmail.com>
2018-10-11 16:09:24 +0300

    Dependency Injection

---
+++ ProjectsManagment/Util/NinjectRegistrations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Ninject.Modules;
+
+namespace ProjectsManagment.Util
+{
+    public class NinjectRegistrations : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IUserRepository>().To<UserRepository>();
+            Bind<IProjectRepository>().To<ProjectRepository>();
+            Bind<IRoleRepository>().To<RoleRepository>();
+            Bind<IScheduleRepository>().To<ScheduleRepository>();
+            Bind<IProjectRoleRepository>().To<ProjectRoleRepository>();
+            Bind<IParticipationTypeRepository>().To<ParticipationTypeRepository>();
+        }
+    }
+}
