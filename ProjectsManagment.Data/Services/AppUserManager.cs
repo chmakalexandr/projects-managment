b4214ce7d66284d5a415cc07bb96b47356396262
chmakalexandr <chmak.av@gmail.com>
2018-11-08 15:06:19 +0300

    Add UserManager

---
+++ ProjectsManagment.Data/Services/AppUserManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Data;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using ProjectsManagment.Data.Repositories;
+using ProjectsManagment.Entity;
+
+namespace ProjectsManagment.Data.Services
+{
+    public class AppUserManager : UserManager<User>
+    {
+        public AppUserManager(IUserStore<User> store) : base(store)
+        {
+        }
+
+        public static AppUserManager Create(IdentityFactoryOptions<AppUserManager> options,
+            IOwinContext context)
+        {
+            
+            AppUserManager manager = new AppUserManager(new UserRepository(context.Get<PMContext>()));
+
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 8,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = true,
+                RequireLowercase = false,
+                RequireUppercase = false,
+            };
+            return manager;
+        }
+    }
+}
