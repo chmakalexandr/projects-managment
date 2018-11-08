49ad0c569b10221d1b67cbf9d60a3938667d3e95
chmakalexandr <chmak.av@gmail.com>
2018-11-08 15:13:29 +0300

    Add Email Service

---
+++ ProjectsManagment.Data/Services/EmailService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectsManagment.Data.Services
+{
+    class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // convert IdentityMessage to a MailMessage
+            var email =
+               new MailMessage(new MailAddress("noreply@mydomain.com", "(do not reply)"),
+               new MailAddress(message.Destination))
+               {
+                   Subject = message.Subject,
+                   Body = message.Body,
+                   IsBodyHtml = true
+               };
+
+            using (var client = new SmtpClient()) // SmtpClient configuration comes from config file
+            {
+                return client.SendMailAsync(email);
+            }
+        }
+    }
+}
