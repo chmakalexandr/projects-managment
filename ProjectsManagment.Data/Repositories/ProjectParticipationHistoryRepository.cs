368daef36bec76c23f456abac3abec3f82c915a9
chmakalexandr <chmak.av@gmail.com>
2018-11-08 15:11:55 +0300

    Add ProjectParticipationHistoryRepository

---
+++ ProjectsManagment.Data/Repositories/ProjectParticipationHistoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.Data;
+using ProjectsManagment.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectsManagment.Data.Repositories
+{
+    public class ProjectParticipationHistoryRepository : EFGenericRepository<ProjectParticipationHistory>
+    {
+        public ProjectParticipationHistoryRepository(PMContext context) : base(context)
+        {
+
+        }
+    }
+   
+}
