f72b5aca97314745010f11015950da9fbb6befe1
alllexxx <freeallex@gmail.com>
2018-11-19 22:25:53 +0300

    Add userController

---
+++ ProjectsManagment.Web/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using ProjectsManagment.Data;
+using ProjectsManagment.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ProjectsManagment.Web.Controllers
+{
+    [RoutePrefix("api/user")]
+    public class UserController : ApiController
+    {
+        private readonly IUserService userService;
+        private readonly IRoleService roleService;
+        public UserController()
+        {
+            userService = new UserService();
+        }
+
+        
+    }
+}
